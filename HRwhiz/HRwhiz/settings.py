a7d9532df1fe924d58e849f5aa0eb34f7b0b7f16
YatinBajajOP <yatin19csu360@ncuindia.edu>
2023-11-04 20:39:21 +0530

    Changed allowed hosts

--- HRwhiz/HRwhiz/settings.py
+++ HRwhiz/HRwhiz/settings.py
@@ -30,10 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG', 'false').lower() == 'true'
+DEBUG = 'RENDER' not in os.environ
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
