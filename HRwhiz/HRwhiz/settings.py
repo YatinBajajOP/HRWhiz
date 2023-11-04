8fd4c49db98129e4b77e95df9ad86c5c8b7f185a
YatinBajajOP <yatin19csu360@ncuindia.edu>
2023-11-04 20:22:31 +0530

    Added hostnames for render

--- HRwhiz/HRwhiz/settings.py
+++ HRwhiz/HRwhiz/settings.py
@@ -32,7 +32,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'false').lower() == 'true'
 DATABASE_URL = os.environ.get('DATABASE_URL')
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
+
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
